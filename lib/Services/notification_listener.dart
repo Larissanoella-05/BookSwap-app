072b9c0502eadcbe541b8621d49e98f66c4c1180
larissanoellaa-05 <s.noella@alustudent.com>
2025-11-09 15:51:15 +0200

    refactor(services): clean up legacy service and provider files

--- lib/Services/notification_listener.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-/// Provider for tracking last seen swap IDs to detect new swaps
-final lastSeenSwapIdsProvider = StateProvider<Set<String>>((ref) => <String>{});
-
-/// Provider for tracking last seen message IDs per chat to detect new messages
-final lastSeenMessageIdsProvider = StateProvider<Map<String, Set<String>>>((ref) => <String, Set<String>>{});
--- lib/Services/profile_providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:bookswap/Services/profile_service.dart';
-
-/// Provider for ProfileService instance (singleton)
-final profileServiceProvider = Provider<ProfileService>((ref) {
-  return ProfileService();
-});
-
